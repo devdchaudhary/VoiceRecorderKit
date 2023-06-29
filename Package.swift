030e7a8e7935fbd317713ed68bdcf4cea3d1c12d
devanshuchaudhary <52855516+devdchaudhary@users.noreply.github.com>
2023-06-29 10:21:37 +0530

    1

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         .target(
             name: "VoiceRecorderPackage",
             dependencies: ["Drops"],
-            resources: [.copy("Sources/Media.xcassets")]
+            resources: [.copy("Media.xcassets")]
         ),
         .testTarget(
             name: "VoiceRecorderPackageTests",
