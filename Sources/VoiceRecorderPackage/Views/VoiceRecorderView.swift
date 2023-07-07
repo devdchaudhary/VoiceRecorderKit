6cb271182fb3c671e7817c224065a5ef63b6b9b7
devanshuchaudhary <52855516+devdchaudhary@users.noreply.github.com>
2023-07-07 11:30:11 +0530

    5

--- Sources/VoiceRecorderPackage/Views/VoiceRecorderView.swift
+++ Sources/VoiceRecorderPackage/Views/VoiceRecorderView.swift
@@ -69,6 +69,7 @@ public struct VoiceRecorderView: View {
                 Spacer()
                 
             }
+            .padding(.horizontal)
             
             Spacer().frame(height: 20)
             
