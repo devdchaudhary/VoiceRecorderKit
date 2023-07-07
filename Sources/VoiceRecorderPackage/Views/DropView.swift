9d6de3c5a0ec7a912de19cb0348752ba2529e3d1
devanshuchaudhary <52855516+devdchaudhary@users.noreply.github.com>
2023-07-07 12:09:12 +0530

    made dropView public.

--- Sources/VoiceRecorderPackage/Views/DropView.swift
+++ Sources/VoiceRecorderPackage/Views/DropView.swift
@@ -47,19 +47,19 @@ public struct DropView {
             }
         }
     
-    static func showWarning(title: String = "An error occured!", subtitle: String = "", titleNoofLines: Int = 0) {
+    public static func showWarning(title: String = "An error occured!", subtitle: String = "", titleNoofLines: Int = 0) {
         
         generator.notificationOccurred(.warning)
         show(title, subtitle, icon:  UIImage(systemName: "exclamationmark.triangle.fill"), position: .top, duration: 2.0, accessibility: "Alert: An error occured!")
     }
     
-    static func showError(title: String = "An error occured!", subtitle: String = "", titleNoofLines: Int = 0) {
+    public static func showError(title: String = "An error occured!", subtitle: String = "", titleNoofLines: Int = 0) {
         
         generator.notificationOccurred(.error)
         show(title, subtitle, icon:  UIImage(systemName: "xmark.circle"), position: .top, duration: 2.0, accessibility: "Alert: An error occured!")
     }
     
-    static func showSuccess(title: String = "Success!", subtitle: String = "", titleNoofLines: Int = 0) {
+    public static func showSuccess(title: String = "Success!", subtitle: String = "", titleNoofLines: Int = 0) {
         
         generator.notificationOccurred(.success)
         show(title, subtitle, icon:  UIImage(systemName: "checkmark.circle.fill"), position: .top, duration: 2.0, accessibility: "Alert: Success!")
